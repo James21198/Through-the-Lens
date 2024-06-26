8bc439a4084e1f72687e4ccfdde04bcc1c5212a0
James Rhodes <james21198@gmail.com>
2024-06-26 19:25:24 +0100

    DB Name Changed in Connection JS

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/through-the-lens');
 
 module.exports = mongoose.connection;
