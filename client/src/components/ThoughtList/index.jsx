b2be4032c2816876a94bcdd17903b6c875b120f8
James Rhodes <james21198@gmail.com>
2024-07-03 20:39:24 +0100

    Banner Images Updated

--- client/src/components/ThoughtList/index.jsx
+++ client/src/components/ThoughtList/index.jsx
@@ -11,12 +11,12 @@ const ThoughtList = ({
   }
 
   return (
-    <div>
+    <>
       {showTitle && <h3>{title}</h3>}
       {thoughts &&
         thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3" style={{ width: '25rem' }}>
-            <img className="card-img-top p-2" src="https://i.pravatar.cc/175?u=${user._id}" alt="Card image cap" />
+            <img className="card-img-top p-2" src={`https://i.pravatar.cc/175?u=${thought.thoughtAuthor}`} alt="Card image cap" />
             <div className="card-body">
               <h5 className="card-title p-2 m-0">
                 {showUsername ? (
@@ -41,7 +41,7 @@ const ThoughtList = ({
             </div>
           </div>
         ))}
-    </div>
+    </>
   );
 };
 
